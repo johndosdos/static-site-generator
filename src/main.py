317d2578a32f0f084a223ca487a0f053f1039841
John <128350391+alpaOo@users.noreply.github.com>
2024-03-13 02:06:35 +0800

    Add src folder with main and textnode py files

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+
+def main():
+    node1 = TextNode("hello world", "bold", "google.com")
+    node2 = TextNode("hello world", "bold", "google.com")
+
+    print(node1)
+
+
+main()
