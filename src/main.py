783cf24304597377f7c6386870193eff9d559ccc
John <128350391+alpaOo@users.noreply.github.com>
2024-03-17 01:58:01 +0800

    improve code formatting

--- src/main.py
+++ src/main.py
@@ -3,6 +3,37 @@ from htmlnode import *
 import re
 
 
+def textnode_to_htmlnode(text_node):
+    """
+    Generate an HTML representation of a text node based on its text_type.
+
+    Parameters:
+        text_node (TextNode): The text node to be converted to HTML.
+
+    Returns:
+        LeafNode: An HTML representation of the text node.
+    """
+    if text_node.text_type == "text":
+        return LeafNode(None, text_node.text, None)
+
+    if text_node.text_type == "bold":
+        return LeafNode("b", text_node.text, None)
+
+    if text_node.text_type == "italic":
+        return LeafNode("i", text_node.text, None)
+
+    if text_node.text_type == "code":
+        return LeafNode("code", text_node.text, None)
+
+    if text_node.text_type == "link":
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+
+    if text_node.text_type == "image":
+        return LeafNode("img", "", {"src": text_node.url, "alt": ""})
+
+    raise Exception("Invalid TextNode text_type")
+
+
 def extract_markdown_links(text):
     pattern = re.compile(r"\[(.*?)\]\((.*?)\)")
     matches = pattern.findall(text)
