3e8f8b316af902f86f2cbcfbeafc484576950de4
John <128350391+alpaOo@users.noreply.github.com>
2024-03-21 14:34:42 +0800

    Implement page generation function
    We need helper functions for our generate_page function for readability.
    The HTML template will be replaced by Markdown contents.

--- src/main.py
+++ src/main.py
@@ -18,8 +18,39 @@ def copy_directory(src_path, dst_path):
             shutil.copy2(new_src, new_dst)
 
 
+def extract_title(markdown):
+    match = re.match(r"^\#\s(.*)", markdown)
+
+    if match:
+        return match.group(1)
+
+
+def modify_template(template, title, content):
+    new_template = template.replace("{{ Title }}", title)
+    new_template = new_template.replace("{{ Content }}", content)
+
+    return new_template
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as file:
+        text = file.read()
+
+    title = extract_title(text)
+    content = markdown_to_htmlnode(text)
+
+    with open(template_path, "r") as file:
+        template = file.read()
+        new_template = modify_template(template, title, content)
+
+    with open(dest_path, "w") as file:
+        file.write(new_template)
+
+
 def main():
-    copy_directory("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
